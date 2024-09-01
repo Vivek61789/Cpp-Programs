e3f220a45a12cf0cb5832e26566671ff45ec3fa9
Vivek617 <vivekreddykeesara@gmail.com>
2024-09-01 19:53:57 +0530

    Add files via upload

---
+++ PrimeOrNot2.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    bool isprime= true;
+    int i,n=13;
+    
+    if(n<2)
+    isprime = false;
+    else{
+        for(i=2;i<n;i++)
+        {
+            if(n%i==0){
+                isprime = false;
+                break;
+            }
+        }
+    }
+    
+    string result=isprime?"prime":"is not prime";
+    cout<<"The number "<<n<<" is: "<<result;
+    
+    return 0;
+    
+}
